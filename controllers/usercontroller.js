984e6366dd2402da656252f377140fce8675d9b9
chefkitten <dgdev@naver.com>
2022-01-17 01:56:13 +0900

    loginProcess add

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -22,3 +22,40 @@ exports.insertUserProcess = (req,res) => {
        }
    })
 }
+
+exports.loginProcess = (req,res) => {
+    let item={
+        'id':req.body.id,
+        'password':req.body.password
+    };
+    User.checkUser(item,(result)=>{
+        if(result===0){
+            res.status(404).send({
+                message:'No matching id, please check your id'
+            });
+        }
+        else if(result===1){
+            res.stauts(401).send({
+                message:'compare server error'
+            });
+        }
+        else if(result===2){
+            User.tokenIssue(item,(token)=>{
+                res.status(201).send({
+                    message:'success',
+                    token
+                });
+            });
+        }
+        else if(result===3){
+            res.status(401).send({
+                message:'password error'
+            });
+        }
+        else{
+            res.status(401).send({
+                message:'Server Error'
+            });
+        } 
+    });
+}
