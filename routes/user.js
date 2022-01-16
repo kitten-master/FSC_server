3c41a8d62ebaf688c6c423d80301f041dca051e2
chefkitten <dgdev@naver.com>
2022-01-16 23:51:47 +0900

    loginProcess add

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/usercontroller');
 const {validateRegister} = require('../middleware/users');
 
 router.post('/register',validateRegister,controller.insertUserProcess);
+router.get('/login',controller.loginProcess);
+
 
 module.exports = router;
